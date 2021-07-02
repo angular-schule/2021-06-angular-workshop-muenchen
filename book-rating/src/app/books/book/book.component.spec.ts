a96930ad16da2ac2605b066136246a21658e50ad
Ferdinand Malcher <ferdinand@malcher.media>
2021-07-02 10:59:38 +0200

    Komponententest

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
 
 import { BookComponent } from './book.component';
 
@@ -17,10 +17,37 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // Input-Property initialisieren
+    component.book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3,
+      price: 5,
+      authors: []
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit an event for doRateUp()', () => {
+    // Arrange
+    let emittedBook: Book | undefined;
+    component.rateUp.subscribe(book => {
+      // EventEmitter arbeitet synchron
+      emittedBook = book;
+    });
+
+    // Act
+    component.doRateUp();
+
+    // Assert
+    expect(emittedBook).toBeTruthy(); // falsy := 0, null, undefined, '', NaN
+    expect(emittedBook).toBe(component.book);
   });
 });
--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let ratingMock: BookRatingService;
 
   beforeEach(async () => {
+    ratingMock = {
+      rateUp: book => book,
+      rateDown: book => book,
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        {
+          provide: BookRatingService,
+          useValue: ratingMock
+        }
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +29,27 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+
+    // Change Detection muss im Tests manuell ausgeführt werden
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('mock should return book', () => {
+    const service = TestBed.inject(BookRatingService);
+    const book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3,
+      price: 10,
+      authors: []
+    };
+
+    const result = service.rateUp(book);
+    expect(result).toBe(book);
   });
 });
