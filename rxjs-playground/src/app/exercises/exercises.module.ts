31591d37db8d6fe3c46c50760c98d485654d2e12
Ferdinand Malcher <ferdinand@malcher.media>
2021-07-03 14:39:24 +0200

    RxJS Playground hinzugefügt

---
+++ rxjs-playground/src/app/exercises/exercises.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { OverviewComponent } from './helpers/overview/overview.component';
+import { HistoryComponent } from './helpers/history/history.component';
+
+import { FromeventComponent } from './fromevent/fromevent.component';
+import { MulticastComponent } from './multicast/multicast.component';
+import { HigherorderComponent } from './higherorder/higherorder.component';
+import { ErrorHandlingComponent } from './error-handling/error-handling.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChatWindowComponent } from './chat/chat-window/chat-window.component';
+import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
+import { CreatingComponent } from './creating/creating.component';
+
+const routes: Routes = [
+  { path: 'exercises', component: OverviewComponent },
+  { path: 'exercises/creating', component: CreatingComponent },
+  { path: 'exercises/fromevent', component: FromeventComponent },
+  { path: 'exercises/multicast', component: MulticastComponent },
+  { path: 'exercises/error-handling', component: ErrorHandlingComponent },
+  { path: 'exercises/unsubscribe', component: UnsubscribeComponent },
+  { path: 'exercises/chat', component: ChatComponent },
+  { path: 'exercises/higherorder', component: HigherorderComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [
+    OverviewComponent,
+    FromeventComponent,
+    MulticastComponent,
+    HistoryComponent,
+    HigherorderComponent,
+    ErrorHandlingComponent,
+    ChatComponent,
+    ChatWindowComponent,
+    UnsubscribeComponent,
+    CreatingComponent,
+  ]
+})
+export class ExercisesModule { }
