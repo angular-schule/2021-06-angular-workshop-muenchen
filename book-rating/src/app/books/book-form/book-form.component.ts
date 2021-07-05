187539492d41616389a93071c7a59faf7d1d15be
Ferdinand Malcher <ferdinand@malcher.media>
2021-07-05 14:07:55 +0200

    Vorbereitung Routen für Suche/Forms

---
+++ book-rating/src/app/books/book-form/book-form.component.ts
@@ -0,0 +1,26 @@
+import { EventEmitter } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Book } from '../shared/book';
+
+
+@Component({
+  selector: 'br-book-form',
+  templateUrl: './book-form.component.html',
+  styleUrls: ['./book-form.component.scss']
+})
+export class BookFormComponent implements OnInit {
+
+  @Output() submitBook = new EventEmitter<Book>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.submitForm(); // TODO
+  }
+
+  submitForm() {
+    this.submitBook.emit({ isbn: 'TEST' } as Book); // Type Assertion
+
+  }
+
+}
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,13 +5,19 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookComponent } from './book/book.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookSearchComponent } from './book-search/book-search.component';
 
 
 @NgModule({
   declarations: [
     BookComponent,
     DashboardComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    BookCreateComponent,
+    BookFormComponent,
+    BookSearchComponent
   ],
   imports: [
     CommonModule,
