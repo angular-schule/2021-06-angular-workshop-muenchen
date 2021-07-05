5426188e790de2db7b5c37ef85d276a3792d32a4
Ferdinand Malcher <ferdinand@malcher.media>
2021-07-05 14:52:25 +0200

    Reactive Forms: Anfang

--- book-rating/src/app/books/book-form/book-form.component.ts
+++ book-rating/src/app/books/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Component, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Book } from '../shared/book';
 
 
@@ -10,11 +11,20 @@ import { Book } from '../shared/book';
 })
 export class BookFormComponent implements OnInit {
 
-  myText = 'Hallo Welt';
+  bookForm: FormGroup;
 
   @Output() submitBook = new EventEmitter<Book>();
 
-  constructor() { }
+  constructor() {
+    // Formular-Modell
+    this.bookForm = new FormGroup({
+      isbn: new FormControl(''),
+      title: new FormControl(''),
+      description: new FormControl(''),
+      price: new FormControl(0),
+      rating: new FormControl(1)
+    });
+  }
 
   ngOnInit(): void {
     this.submitForm(); // TODO
