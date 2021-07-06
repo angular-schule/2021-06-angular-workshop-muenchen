4894023a83d97e324fe1e86e357eaab12f4deced
Ferdinand Malcher <ferdinand@malcher.media>
2021-07-06 11:25:15 +0200

    Reactive Forms

--- book-rating/src/app/books/book-form/book-form.component.ts
+++ book-rating/src/app/books/book-form/book-form.component.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Component, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Book } from '../shared/book';
 
 
@@ -18,21 +18,54 @@ export class BookFormComponent implements OnInit {
   constructor() {
     // Formular-Modell
     this.bookForm = new FormGroup({
-      isbn: new FormControl(''),
-      title: new FormControl(''),
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+      ]),
+      title: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(255)
+      ]),
       description: new FormControl(''),
-      price: new FormControl(0),
-      rating: new FormControl(1)
-    });
+      price: new FormControl(0, [
+        Validators.min(0)
+      ]),
+      rating: new FormControl(1, [
+        Validators.min(1),
+        Validators.max(5),
+      ])
+    });  
   }
 
-  ngOnInit(): void {
-    this.submitForm(); // TODO
-  }
+  ngOnInit(): void {}
 
   submitForm() {
-    this.submitBook.emit({ isbn: 'TEST' } as Book); // Type Assertion
+    /*
+    // Alternative:
+    if (this.bookForm.invalid) {
+      this.bookForm.markAllAsTouched();
+      return;
+    }*/
 
+    console.log('submit');
+
+    const book = {
+      ...this.bookForm.value,
+      authors: []
+    };
+
+    this.submitBook.emit(book);
+  }
+
+  isInvalid(controlName: string): boolean {
+    const control = this.bookForm.get(controlName);
+    return !!control && control.touched && control.invalid;
+  }
+
+  hasError(controlName: string, error: string): boolean {
+    const control = this.bookForm.get(controlName);
+    return !!control && control.touched && control.hasError(error);    
   }
 
 }
